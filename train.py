fadea901a7a43b13721aa2000b9fd1690c33872c
Elanmarkowitz <elanmarkowitz@ucla.edu>
2019-12-01 18:51:55 -0800

    train update

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ def main(args):
     val_dataloader = get_data_loader('augmentation_data/val_documents.txt')
     test_dataloader = get_data_loader('augmentation_data/test_documents.txt')
 
-    trainer.train(train_dataloader, val_dataloader, debias_method=args.m, epochs=args.e, )
+    trainer.train(train_dataloader, val_dataloader, debias_method=args.m, epochs=args.e, checkpoint_dir=args.checkpoint_dir)
 
 
 
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', type=str, help='debias method to use: active, both, orig')
     parser.add_argument('-e', type=int, help='epochs')
-    parser.add_argument('--checkpoint-dir', type=str, default='')
+    parser.add_argument('--checkpoint-dir', type=str, default='checkpoints')
     args = parser.parse_args()
     main(args)
