cc607a3eff4e38b2daaecb452dbcefedd4e16af5
Elanmarkowitz <elanmarkowitz@ucla.edu>
2019-12-02 01:43:44 -0800

    fix multiple alias bug

--- coref_resolver.py
+++ coref_resolver.py
@@ -67,6 +67,8 @@ class CorefResolver:
                     for span_idx in [orig_span1_end, orig_span1_start, orig_span2_end, orig_span2_start]:
                         if span_idx < 0 or span_idx > len(combined_orig_label_seq):
                             import pdb; pdb.set_trace()
+                            s = data_utils.get_tokenized_versions(self.tokenizer, sentence)
+                            data_utils.get_sentence_spans(self.tokenizer, sentence)
                     for span_idx in [swap_span1_end, swap_span1_start, swap_span2_end, swap_span2_start]:
                         if span_idx < 0 or span_idx > len(combined_swap_label_seq):
                             import pdb; pdb.set_trace()
